60172990729cd4944b8d28cb96ae83ee1ab032aa
Ryan Brooks <rbrooks@Ryans-MacBook-Pro.local>
2018-01-10 15:02:13 -0700

    spent a lot of time getting eslint figured out and SublimeLinter

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+var OFF = 0,
+  WARN = 1,
+  ERROR = 2;
+
+module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2016,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    'space-before-function-paren': [ERROR, 'never'],
+    'eol-last': OFF,
+    'comma-dangle': ['error', { functions: 'never' }],
+    'func-names': [ERROR, 'as-needed']
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  }
+};
