f4788f67cf0ddfec472a348ed052a2e89297e11e
TheBrooks <the.ryan.brooks@gmail.com>
2018-01-12 12:12:33 -0700

    added flow to tests

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import { shallow } from 'enzyme';
 import preload from '../../data.json';
@@ -5,17 +7,17 @@ import Search from '../Search';
 import ShowCard from '../ShowCard';
 
 test('Search renders correctly', () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   expect(component).toMatchSnapshot();
 });
 
 test('Search should render correct amount of shows', () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
 test('Search should render correct amount of show based on search term', () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   const searchWord = 'black';
   component.find('input').simulate('change', { target: { value: searchWord } });
   const showCount = preload.shows.filter(
